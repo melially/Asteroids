10faab73267bc7feec89b4f60aaf2e769e881bb7
melially <mat.levasseur@outlook.com>
2025-02-27 16:59:49 -0500

    Black Screen

--- main.py
+++ main.py
@@ -2,9 +2,19 @@ import pygame # this allows us to use code from the open-source pygame library t
 from constants import *
 
 def main():
+    pygame.init()
     print("Starting Asteroids!")
     print("Screen width: 1280")
     print("Screen height: 720")
+    
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
